215310b1557758d8dccd4819d08082a2a47c6011
Santiago Ramirez Forero <santiforero57@gmail.com>
2024-04-16 22:06:19 -0500

    cambios

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es" className="scroll-smooth">
       <body>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
